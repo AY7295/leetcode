a9e9590575639d5d6ea24e3b28c5ebded27e6888
AY <Adolph7295@outlook.com>
2022-10-26 12:36:52 +0800

    add official answer for NO.189

--- 189.轮转数组.cpp
+++ 189.轮转数组.cpp
@@ -8,25 +8,45 @@ using namespace std;
 // @lc code=start
 class Solution
 {
-public:
-    void rotate(vector<int> &nums, int k)
+    void reverse(vector<int> &nums, int start, int end)
     {
-        int len = nums.size();
-        k = k % len;
-        vector<int> temp;
-        temp.reserve(k);
-        for (int i = k; i > 0; i--)
+        while (start < end)
         {
-            temp.push_back(nums[len - i]);
-        }
-        for (int i = len - 1; i >= k; i--)
-        {
-            nums[i] = nums[i - k];
-        }
-        for (int i = 0; i < k; i++)
-        {
-            nums[i] = temp[i];
+            swap(nums[start], nums[end]);
+            start++;
+            end--;
         }
     }
+
+public:
+    // descp official
+    void rotate(vector<int> &nums, int k)
+    {
+        k %= nums.size();
+        reverse(nums, 0, nums.size() - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.size() - 1);
+    }
+
+    // descp mine
+    // void rotate(vector<int> &nums, int k)
+    // {
+    //     int len = nums.size();
+    //     k = k % len;
+    //     vector<int> temp;
+    //     temp.reserve(k);
+    //     for (int i = k; i > 0; i--)
+    //     {
+    //         temp.push_back(nums[len - i]);
+    //     }
+    //     for (int i = len - 1; i >= k; i--)
+    //     {
+    //         nums[i] = nums[i - k];
+    //     }
+    //     for (int i = 0; i < k; i++)
+    //     {
+    //         nums[i] = temp[i];
+    //     }
+    // }
 };
 // @lc code=end
