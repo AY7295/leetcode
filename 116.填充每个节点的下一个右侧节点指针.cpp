de2ead04db07fcc24b3dac60b95b13caa92e43c2
AY <Adolph7295@outlook.com>
2022-11-01 11:11:18 +0800

    chore: add extra solution for NO.116

--- 116.填充每个节点的下一个右侧节点指针.cpp
+++ 116.填充每个节点的下一个右侧节点指针.cpp
@@ -4,6 +4,7 @@
  * [116] 填充每个节点的下一个右侧节点指针
  */
 #include <algorithm>
+#include <queue>
 using namespace std;
 
 // Definition for a Node.
@@ -29,28 +30,45 @@ class Solution
 {
 
 public:
+    // descp most voted solution
     Node *connect(Node *root)
     {
-        if (root == nullptr)
-            return root;
-
-        function<void(Node *, Node *, Node *)> help = [&](Node *root, Node *right, Node *left)
+        Node *pre = root, *cur;
+        while (pre)
         {
-            if (right == nullptr)
-                return;
-
-            left->next = right;
-            right->next = root->next == nullptr ? nullptr : root->next->left;
-
-            help(right, right->right, right->left);
-            help(left, left->right, left->left);
-            return;
-        };
-
-        root->next = nullptr;
-        help(root, root->right, root->left);
-
+            cur = pre;
+            while (cur && cur->left)
+            {
+                cur->left->next = cur->right;
+                if (cur->next)
+                {
+                    cur->right->next = cur->next->left;
+                }
+                cur = cur->next;
+            }
+            pre = pre->left;
+        }
         return root;
     }
+
+    // descp mine
+    // Node *connect(Node *root)
+    // {
+    //     if (root == nullptr)
+    //         return root;
+
+    //     if (root->left)
+    //     {
+    //         root->left->next = root->right;
+    //         if (root->next)
+    //         {
+    //             root->right->next = root->next->left;
+    //         }
+    //         connect(root->left);
+    //         connect(root->right);
+    //     }
+
+    //     return root;
+    // }
 };
 // @lc code=end
